8c2b287fe3405fa9ddf5f1391d405ea1fca1a1f2
Rob Halff <rob.halff@gmail.com>
2014-02-20 16:54:33 +0100

    update

--- nodes/stream/node.js
+++ nodes/stream/node.js
@@ -1,5 +1,5 @@
 // ok streams should be installed async
-if(Object.keys(input.options).length) {
+if(input.options && Object.keys(input.options).length) {
   output = [input.twitter, 'stream', input.endpoint, input.options]
 } else {
   output = [input.twitter, 'stream', input.endpoint]
