0779f7eb2c7612866215341508fcbbf8e8893bce
Rob Halff <rob.halff@gmail.com>
2014-12-28 22:29:46 +0100

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  api: require('./api'),
+  onData: require('./onData'),
+  search: require('./search'),
+  statusesFilter: require('./statusesFilter'),
+  statusesSample: require('./statusesSample'),
+  stream: require('./stream'),
+  updateStatus: require('./updateStatus'),
+  user: require('./user'),
+  verifyCredentials: require('./verifyCredentials')
+}
